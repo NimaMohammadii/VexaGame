bed1586212fa458a547693099af443ef84b5b2de
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-09-08 13:23:35 +0330

    Fix Crash rocket model export

--- src/miniapp/crash/index.ts
+++ src/miniapp/crash/index.ts
@@ -1,3 +1,3 @@
-export { CRASH_SECTION } from './section';
+export { CRASH_SECTION, CRASH_ROCKET_MODEL_URL } from './section';
 export { CRASH_STYLES } from './styles';
 export { CRASH_SCRIPT } from './scripts/main';
